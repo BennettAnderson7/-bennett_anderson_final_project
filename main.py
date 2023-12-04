2df20460b5827bd6315ec2a535fceddd3679f060
BennettAnderson7 <bennett.anderson24@bcp.org>
2023-12-04 11:12:28 -0800

    pygame setup

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 # File created by: Bennett Anderson
 
+# Project Title: "(Name TBD) Platformer"
+
+'''
+Goals:
+- Create a mario style platformer 
+- Include side scrolling, powerups, mobs, challenges, and levels
+- Implement textures and sounds
+- Have it work perfectly based on my limited knowledge
+
+'''
+
+import pygame, sys
+from settings import *
+
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+screen.fill('black')
