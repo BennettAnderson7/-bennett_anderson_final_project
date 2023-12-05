9afa83846312b7f455ee7d7690877e0c8221c7e3
BennettAnderson7 <bennett.anderson24@bcp.org>
2023-12-05 11:36:03 -0800

    Added level display, textures

--- tiles.py
+++ tiles.py
@@ -1,10 +1,16 @@
 import pygame
 from settings import *
+import os
 
+# setup asset folders here - images sounds etc.
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'images')
+snd_folder = os.path.join(game_folder, 'sounds')
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,size):
         super().__init__()
         self.image = pygame.Surface((size,size))
-        self.image.fill('green')
+        #self.image.fill('green')
+        self.image = pygame.image.load(os.path.join(img_folder, 'mario_grass_tile.png')).convert()
         self.rect = self.image.get_rect(topleft = pos)
